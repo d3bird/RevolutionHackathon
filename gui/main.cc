1ba3efb7e2b0a0219bb5ea489da9d2b886e59479
Scott Crawford <scrawfor@odd03.cs.ohiou.edu>
2019-03-03 00:53:25 -0500

    added scrolling to the output

--- gui/main.cc
+++ gui/main.cc
@@ -55,24 +55,59 @@ for (int i=0;i<ymax && i<coms.size();i++){
 return 2;// returns the input
 }
 
-int printoutput(int start,int linemerge, vector<string> &lines){
+
+
+
+int printoutput(int start,int linemerge, vector<string> &lines, vector<string> &his,int ymax, int xmax){
 int end = start;
 
-for(int i =0; i <lines.size();i++){
-	move(end,linemerge+1);
-//	if(lines[i].size()<(xmax-linemerge)){
-        printw(lines[i].c_str());
+if(lines.size()+his.size()>ymax){
+int diff = lines.size()+his.size()-ymax;
+int temp;
+	for(int i =0; i < lines.size(); i++){//adding the new lines to the history
+		his.push_back(lines[i]);
+	}
+	for(int i =0 ; i < diff;i++){//removing the old commands
+		his.erase(his.begin());
+	}
+
+	for(int y =1; y<ymax;y++){
+		for(int x=linemerge+1; x<xmax;x++){
+		move(y,x);
+		echochar(' ');
+		}
+		move(y,linemerge+1);
+		printw(his[y-1].c_str());
+	}
+//	for(int y =1; y<ymax;y++){// recreating the output
+		
+		//move(y,temp);
+		//printw(his[y-1].c_str());
 //	}
+}else{
 
+for(int i =0; i <lines.size();i++){
+	if(end<xmax){
+	move(end,linemerge+1);
+        printw(lines[i].c_str());
 	end++;
+	his.push_back(lines[i]);
+	}else{
+//	for(int y =0;y<ymax;y++){
+	//	for(int x = linemerge+1;x<xmax;x++){
+			
+	//	}
+	}
+
 }
-
-
+}
 return end;
 }
 
 int main(){
  vector<command> commands;
+ vector<string> outputhis;
+ vector<string> inputhis;
  command temp("create command");
  commands.push_back(temp);
  temp.setstring("command 2");
@@ -100,7 +135,7 @@ commands.push_back(temp);
  int linemark = w/2;
  move(0,0);
 //attron(A_BOLD);
- printw("custom command list");
+ printw("custom command list:");
  move(0,linemark+1);
  printw("output");
  for(int i =0 ;i< h;i++){
@@ -109,9 +144,12 @@ commands.push_back(temp);
  }
 int termpoint =h-3;
 move(termpoint, 0);
-printw("termanal input");
+printw("termanal input:");
 termpoint++;
 
+move(termpoint-7,0);
+int comh= termpoint -7;
+printw("command history:");
 
 move(2,0);
 int endpos = createInputside(linemark, h,  commands);
@@ -171,7 +209,7 @@ while (running){
 	//	string str(inputcommand.begin(),inputcommand.end());
 	//	printw("enter was clicked");
 		 temps ="";
-		 if(!inputcommand.empty()){
+		 if(!inputcommand.empty()){// if there is a consol cmand
 		 move(termpoint,compos);
 		 for(int a = 0 ; a < inputcommand.size();a++){
 			temps.push_back(inputcommand[a]);
@@ -179,15 +217,18 @@ while (running){
 			
 			 echochar(' ');
 		 }
-		 
-		lines.push_back(temps);	 
-		outputpos= printoutput(outputpos ,linemark,lines);
+		lines.push_back(temps);	
+		outputpos= printoutput(outputpos ,linemark,lines,outputhis,h,w);
 		lines.resize(0);
 		inputcommand.resize(0);
 		compos =0;
+		
+		 }else{//run the menu command
+
+
 		 }
 		 break;
-	 case KEY_BACKSPACE:
+               	 case KEY_BACKSPACE:
 		 if(!inputcommand.empty()){
 		 inputcommand.pop_back();
 		 compos--;
