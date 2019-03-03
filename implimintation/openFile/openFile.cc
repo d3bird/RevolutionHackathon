16742b4a5957b681cb6cb2f48025519e86292dbd
Bain <an021916@ohio.edu>
2019-03-03 01:34:42 -0500

    Optimized code

--- implimintation/openFile/openFile.cc
+++ implimintation/openFile/openFile.cc
@@ -1,23 +1,7 @@
 #include <unistd.h>
-#include <sys/wait.h>
-#include <iostream>
-#include <mutex>
 using namespace std;
 
 int main()
 {
-  std::mutex test;
-  test.lock();
-  cout << "This is the parent! Here, meet my child.";
-  pid_t pid = vfork();
-  if(pid == 0)
-  {
-    execl("./b.out", "/.", (char *)NULL);
-    test.unlock();
-  }
-  else
-  {
-    wait(0);
-    cout << "Back to the parent!\n";
-  }
+  execl("./b.out", "/.", (char *)NULL);
 }
